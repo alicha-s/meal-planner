ba0bad2cc50274d5c9be26df81f35f5227f4aaba
alicha-s <alicha-sangkajan@pro-vision.jp>
2026-05-28 18:28:35 +0900

    change user input validation method

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-from pydantic import BaseModel,ValidationError
 from typing import Literal
 import sys,os
 
-class User(BaseModel):
-    name: str
-    age: int
-    weight: int
-    height: int
-    sex: Literal["male","female"]
-    activity: Literal["Sedentary","Lightly Active","Moderately Active","Very Active"]
-    goal: Literal["lose weight","maintain","gain weight"]
-    target: float
-
-    @classmethod
-    def get_input(cls):
-        while True:
-            user_input = {"name":"Name: ",
-                "age":"Age: ",
-                "weight":"Weight(kg): ",
-                "height":"Height(cm): ",
-                "sex":"Sex(male/female): ",
-                "activity":"Activity(Sedentary/Lightly Active/Moderately Active/Very Active): ",
-                "goal":"Goal(lose weight/maintain/gain weight): ",
-                "target":"weekly target(kg): "}
-            
-            args = {}
-            for k,v in user_input.items():
-                args[k] = input(v)
-            
-            try:
-                user = cls(**user_input)
-                return user
-            except ValidationError as e:
-                print(e)
+class User:
+    def __init__(self,name,age,weight,height,sex,activity,goal,target):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.sex = sex
+        self.activity = activity
+        self.goal = goal
+        self.target = target
 
     @property
     def bmr(self):
@@ -43,13 +21,13 @@ class User(BaseModel):
         
     @property
     def maintenace_tdee(self):
-        if self.activity == "Sedentary":
+        if self.activity == 1:
             return round(self.bmr * 1.2)
-        elif self.activity == "Lightly Active":
+        elif self.activity == 2:
             return round(self.bmr * 1.375)
-        elif self.activity == "Moderately Active":
+        elif self.activity == 3:
             return round(self.bmr * 1.55)
-        elif self.activity == "Very Active":
+        elif self.activity == 4:
             return round(self.bmr * 1.725)
     
     @property
@@ -72,7 +50,38 @@ class User(BaseModel):
         return TC
 
 def create_user():
-    user = User.get_input()
+    user_input = {"name":[str,"Name: "],
+        "age":[int,"Age: "],
+        "weight":[int,"Weight(kg): "],
+        "height":[int,"Height(cm): "],
+        "sex":[str,"Sex(male/female): "],
+        "activity":[int,"Activity(Please input number 1~4\nSedentary= 1/Lightly Active= 2/Moderately Active= 3/Very Active= 4): "],
+        "goal":[int,"Goal(Please input number 1~3\nlose weight= 1/maintain= 2/gain weight= 3): "],
+        "target":[float,"weekly target(kg): "]}
+
+    for k,v in user_input.items():
+        valid = True
+        while valid:
+            try:
+                user_input[k] = check_validation(k,v[0],input(v[1]))
+                valid = False
+            except:
+                print("Invalid Input. Try agian.")
+
+    user = User(**user_input)
+
+def check_validation(key,val_type,value):  
+    if key == "sex":
+        if val_type(value) not in ["male","female"]:
+            raise ValueError
+    elif key == "activity":
+        if val_type(value) not in [1,2,3,4]:
+            raise ValueError
+    elif key == "goal":
+        if val_type(value) not in [1,2,3]:
+            raise ValueError
+    else:
+        return val_type(value)
 
 def main():
     create_user()
